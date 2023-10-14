147a78266ac3c5f7d6ebb1062f456c78c3e2e487
Kim Hayeon <k.hayeon00@gmail.com>
2023-10-14 19:59:03 +0900

    :art: Design: 스플래시 완료(#6)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import GlobalStyle from './GlobalStyle';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Splash from './pages/splash/Splash';
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Splash />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
