83d0ed75fd5ff77b054820d95fa47b4bad30347a
yonainthefish <yonaisgood@gmail.com>
2023-10-16 00:15:29 +0900

    :writing_hand:Rename: Button 컴포넌트 폴더 변경 후 경로 수정

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,17 +1,15 @@
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import StyledInput from '../components/StyledInput';
-import StyledAuth from '../components/StyledAuth';
-import Button from '../components/Button';
-import ProfileBasicImg from '../asset/image/profile-basic-img.svg';
-import EditCircle from '../asset/icon/EditCircle.svg';
-import Logo from '../asset/icon/Logo.svg';
+import {Link} from "react-router-dom";
+import {useState} from "react";
+import StyledInput from "../components/StyledInput";
+import StyledAuth from "../components/StyledAuth";
+import Button from "../components/Button/Button";
+import ProfileBasicImg from "../asset/image/profile-basic-img.svg";
+import EditCircle from "../asset/icon/EditCircle.svg";
+import Logo from "../asset/icon/Logo.svg";
 
 export default function Signup() {
   const [disabled, setDisabled] = useState(true);
-  const [clientWitch, setClientWitch] = useState(
-    document.documentElement.clientWidth
-  );
+  const [clientWitch, setClientWitch] = useState(document.documentElement.clientWidth);
 
   return (
     <StyledAuth>
@@ -19,7 +17,7 @@ export default function Signup() {
         {clientWitch < 431 && (
           <>
             <h1>
-              <img src={Logo} alt='로고' />
+              <img src={Logo} alt="로고" />
             </h1>
             <p>
               안녕하세요.
@@ -29,41 +27,33 @@ export default function Signup() {
           </>
         )}
         <article>
-          <Link to='/login'>Login</Link>
+          <Link to="/login">Login</Link>
           <h2>Signup</h2>
         </article>
         <form>
-          <label htmlFor='profile-inp' className='profile'>
-            <img src={ProfileBasicImg} alt='프로필 사진' />
-            <img src={EditCircle} alt='변경하기' />
+          <label htmlFor="profile-inp" className="profile">
+            <img src={ProfileBasicImg} alt="프로필 사진" />
+            <img src={EditCircle} alt="변경하기" />
           </label>
-          <input id='profile-inp' type='file' className='a11y-hidden' />
+          <input id="profile-inp" type="file" className="a11y-hidden" />
 
-          <label htmlFor='username-inp' className='a11y-hidden'>
+          <label htmlFor="username-inp" className="a11y-hidden">
             사용자 이름
           </label>
-          <StyledInput id='username-inp' placeholder='username' type='text' />
-          <label htmlFor='email-inp' className='a11y-hidden'>
+          <StyledInput id="username-inp" placeholder="username" type="text" />
+          <label htmlFor="email-inp" className="a11y-hidden">
             이메일
           </label>
-          <StyledInput id='email-inp' placeholder='email' type='email' />
-          <label htmlFor='password-inp' className='a11y-hidden'>
+          <StyledInput id="email-inp" placeholder="email" type="email" />
+          <label htmlFor="password-inp" className="a11y-hidden">
             비밀번호
           </label>
-          <StyledInput
-            id='password-inp'
-            placeholder='password'
-            type='password'
-          />
-          <label htmlFor='password-inp' className='a11y-hidden'>
+          <StyledInput id="password-inp" placeholder="password" type="password" />
+          <label htmlFor="password-inp" className="a11y-hidden">
             비밀번호 재확인
           </label>
-          <StyledInput
-            id='password-inp'
-            placeholder='password confirm'
-            type='password'
-          />
-          <Button size={clientWitch > 1024 ? 'l' : 's'} disabled={disabled}>
+          <StyledInput id="password-inp" placeholder="password confirm" type="password" />
+          <Button size={clientWitch > 1024 ? "l" : "s"} disabled={disabled}>
             Signup
           </Button>
         </form>
