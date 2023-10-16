8b972edc28af806d0fb97c7c9e364657338fcf71
Kim Hayeon <k.hayeon00@gmail.com>
2023-10-16 16:11:09 +0900

    :lipstick: Style: 프리티어 적용

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,13 +1,13 @@
-import React, {useState} from "react";
-import AccordionWrapper from "./AccordionStyle";
-import Direction from "../../asset/icon/Arrow.svg";
+import React, { useState } from 'react';
+import AccordionWrapper from './AccordionStyle';
+import Direction from '../../asset/icon/Arrow.svg';
 
 interface AccordionProps {
   question: string;
   answer: string;
 }
 
-function Accordion({question, answer}: AccordionProps) {
+function Accordion({ question, answer }: AccordionProps) {
   const [isAccordionOpen, setIsAccordionOpen] = useState(false);
 
   const handleQuestionClick = () => {
@@ -16,18 +16,22 @@ function Accordion({question, answer}: AccordionProps) {
 
   return (
     <AccordionWrapper>
-      <div id="Accordion_wrap">
-        <div className="que" onClick={handleQuestionClick}>
+      <div id='Accordion_wrap'>
+        <div className='que' onClick={handleQuestionClick}>
           <span>{question}</span>
-          <div className="arrow-wrap">
-            <span className={isAccordionOpen ? "arrow-top" : "arrow-bottom"}>
-              <img className="directionIcon" src={Direction} alt="Arrow Direction" />
+          <div className='arrow-wrap'>
+            <span className={isAccordionOpen ? 'arrow-top' : 'arrow-bottom'}>
+              <img
+                className='directionIcon'
+                src={Direction}
+                alt='Arrow Direction'
+              />
             </span>
           </div>
         </div>
         {isAccordionOpen && (
-          <div className="anw">
-            {answer.split(",").map((imagePath, index) => (
+          <div className='anw'>
+            {answer.split(',').map((imagePath, index) => (
               <img key={index} src={imagePath.trim()} alt={`이미지 ${index}`} />
             ))}
           </div>
