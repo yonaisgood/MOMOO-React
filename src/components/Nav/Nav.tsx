7223eb2991032463d1419d91eb87dcdb265ff65b
yonainthefish <yonaisgood@gmail.com>
2023-10-23 23:21:01 +0900

    :bulb: Feat: Nav-> My, Upload 모달 작동 코드 추가

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,13 +1,46 @@
 import StyledNav from './StyledNav';
 import MyPopup from '../MyPopup/MyPopup';
+import Upload from '../Upload/Upload';
 import { useState } from 'react';
 
 export default function Nav() {
-  const [openPopup, setOpenPopup] = useState(true);
+  const [openUploadModal, setOpenUploadModal] = useState(false);
+  const [openMyModal, setOpenMyModal] = useState(false);
+
+  const openUploadModalFunc = () => {
+    setOpenUploadModal(true);
+    document.body.classList.add('modal-open');
+  };
+
+  const closeUploadModal = () => {
+    setOpenUploadModal(false);
+    document.body.classList.remove('modal-open');
+  };
+
+  const openMyModalFunc = () => {
+    setOpenMyModal(true);
+  };
+
+  const closeMyModal = () => {
+    setOpenMyModal(false);
+  };
 
   return (
     <StyledNav>
-      {openPopup && <MyPopup setOpenPopup={setOpenPopup} />}
+      <div className="navBtn">
+        <button className="my" onClick={openMyModalFunc}>
+          My
+        </button>
+        <button className="upload" onClick={openUploadModalFunc}>
+          Upload
+        </button>
+      </div>
+      {openUploadModal && (
+        <div className="modal-overlay">
+          <Upload setOpenPopup={closeUploadModal} />
+        </div>
+      )}
+      {openMyModal && <MyPopup setOpenPopup={closeMyModal} />}
     </StyledNav>
   );
 }
