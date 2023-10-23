8a0822be22f4bef35153fe53f1c1db4168b6de89
yonainthefish <yonaisgood@gmail.com>
2023-10-23 23:29:31 +0900

    :teddy_bear: Chore: 필요 없는 코드 삭제

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -3,7 +3,7 @@ import Splash from '../pages/splash/Splash';
 import Login from '../pages/Login';
 import Signup from '../pages/Signup';
 import Setting from '../pages/Setting/Setting';
-import Upload from '../pages/Upload/Upload';
+import Upload from '../components/Upload/Upload';
 import KakaoMap from '../components/Map/KakaoMap';
 import My from '../pages/My/My';
 import NavRoute from './NavRoute';
@@ -13,7 +13,7 @@ import { AuthRoute, NonAuthRoute } from './AuthRoute';
 
 export default function Router() {
   const [clientWitch, setClientWitch] = useState(
-    document.documentElement.clientWidth
+    document.documentElement.clientWidth,
   );
 
   useEffect(() => {
@@ -26,23 +26,21 @@ export default function Router() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Splash />}></Route>
+          <Route path="/" element={<Splash />}></Route>
 
           <Route element={<NonAuthRoute />}>
             <Route element={clientWitch > 430 ? <NavRoute /> : <Outlet />}>
-              <Route path='/login' element={<Login />}></Route>
-              <Route path='/signup' element={<Signup />}></Route>
+              <Route path="/login" element={<Login />}></Route>
+              <Route path="/signup" element={<Signup />}></Route>
             </Route>
           </Route>
 
           <Route element={<AuthRoute />}>
             <Route element={<NavRoute />}>
-              <Route path='/home' element={<Home />}></Route>
-              <Route path='/setting' element={<Setting />}></Route>
-              <Route path='/my' element={<My />}></Route>
+              <Route path="/home" element={<Home />}></Route>
+              <Route path="/setting" element={<Setting />}></Route>
+              <Route path="/my" element={<My />}></Route>
             </Route>
-            <Route path='/upload' element={<Upload />}></Route>
-            <Route path='/kakaoMap' element={<KakaoMap />}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
