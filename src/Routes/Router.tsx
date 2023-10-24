49a4a30ee803ca615e185844bd19c1490b2da72e
Kim Hayeon <k.hayeon00@gmail.com>
2023-10-24 15:45:51 +0900

    :bulb: Feat: 이용약관/개인정보처리방침 라우팅 추가

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import Splash from '../pages/splash/Splash';
 import Login from '../pages/Login';
 import Signup from '../pages/Signup';
@@ -7,13 +8,14 @@ import Upload from '../pages/Upload/Upload';
 import KakaoMap from '../components/Map/KakaoMap';
 import My from '../pages/My/My';
 import NavRoute from './NavRoute';
-import { useEffect, useState } from 'react';
 import Home from '../pages/Home/Home';
+import Terms from '../pages/Policy/Terms';
+import PrivacyPolicy from '../pages/Policy/PrivacyPolicy';
 import { AuthRoute, NonAuthRoute } from './AuthRoute';
 
 export default function Router() {
   const [clientWitch, setClientWitch] = useState(
-    document.documentElement.clientWidth
+    document.documentElement.clientWidth,
   );
 
   useEffect(() => {
@@ -26,23 +28,27 @@ export default function Router() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Splash />}></Route>
+          <Route path="/" element={<Splash />}></Route>
+          <Route element={<NavRoute />}>
+            <Route path="/terms" element={<Terms />}></Route>
+            <Route path="/policy" element={<PrivacyPolicy />}></Route>
+          </Route>
 
           <Route element={<NonAuthRoute />}>
             <Route element={clientWitch > 430 ? <NavRoute /> : <Outlet />}>
-              <Route path='/login' element={<Login />}></Route>
-              <Route path='/signup' element={<Signup />}></Route>
+              <Route path="/login" element={<Login />}></Route>
+              <Route path="/signup" element={<Signup />}></Route>
             </Route>
           </Route>
 
           <Route element={<AuthRoute />}>
             <Route element={<NavRoute />}>
-              <Route path='/home' element={<Home />}></Route>
-              <Route path='/setting' element={<Setting />}></Route>
-              <Route path='/my' element={<My />}></Route>
+              <Route path="/home" element={<Home />}></Route>
+              <Route path="/setting" element={<Setting />}></Route>
+              <Route path="/my" element={<My />}></Route>
             </Route>
-            <Route path='/upload' element={<Upload />}></Route>
-            <Route path='/kakaoMap' element={<KakaoMap />}></Route>
+            <Route path="/upload" element={<Upload />}></Route>
+            <Route path="/kakaoMap" element={<KakaoMap />}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
