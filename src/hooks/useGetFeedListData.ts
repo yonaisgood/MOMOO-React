9748178acf43101b74c0e007b720bd972aa5e094
Kim Hayeon <k.hayeon00@gmail.com>
2023-11-10 07:30:27 +0900

    :bulb: Feat: 앨범 상세 데이터 받아서 뿌리기(#56)

---
+++ src/hooks/useGetFeedListData.ts
@@ -0,0 +1,39 @@
+import { appFireStore } from '../firebase/config';
+import {
+  getDocs,
+  collection,
+  query,
+  where,
+  or,
+  Timestamp,
+  DocumentData,
+} from 'firebase/firestore';
+import useAuthContext from '../hooks/useAuthContext';
+
+export default function useGetFeedListData() {
+  const { user } = useAuthContext();
+
+  const getFeedListData = async (feedList: string[]) => {
+    if (user === null) {
+      return;
+    }
+
+    try {
+      const feedRef = collection(appFireStore, user.uid, user.uid, 'feed');
+      const searchList = feedList.map((feedId) => where('id', '==', feedId));
+      const q = query(feedRef, or(...searchList));
+      const querySnapshot = await getDocs(q);
+      const feedListData: DocumentData[] = [];
+
+      querySnapshot.forEach((doc) => {
+        feedListData.push(doc.data());
+      });
+
+      return feedListData;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return getFeedListData;
+}
