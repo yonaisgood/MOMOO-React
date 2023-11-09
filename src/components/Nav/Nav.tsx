5ac4bc5ace5c9ddc47a28a4d7c285cbf565b3b57
Kim Hayeon <k.hayeon00@gmail.com>
2023-11-10 08:28:41 +0900

    :art: Design: 홈 모바일 헤더로 컴포넌트 분리(#65)

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -5,12 +5,21 @@ import HomeImg from '../../asset/icon/HomePc.svg';
 import LogoImg from '../../asset/icon/Logo.svg';
 import UploadImg from '../../asset/icon/UploadPc.svg';
 import MypageImg from '../../asset/icon/ProfilePc.svg';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Nav() {
   const [openUploadModal, setOpenUploadModal] = useState(false);
   const [openMyModal, setOpenMyModal] = useState(false);
+  const [clientWitch, setClientWitch] = useState(
+    document.documentElement.clientWidth,
+  );
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      setClientWitch(document.documentElement.clientWidth);
+    });
+  }, []);
 
   const openUploadModalFunc = () => {
     setOpenUploadModal(true);
@@ -22,28 +31,36 @@ export default function Nav() {
   };
 
   return (
-    <StyledNav>
-      <div className="navBtn">
-        <Link to="/home">
-          <button className="home">
-            <img src={HomeImg} alt="홈 아이콘" />
-            <p>Home</p>
-          </button>
-        </Link>
-        <button className="upload" onClick={openUploadModalFunc}>
-          <img src={UploadImg} alt="업로드 아이콘" />
-          <p>Upload</p>
-        </button>
-        <button className="my" onClick={openMyModalFunc}>
-          <img src={MypageImg} alt="마이페이지 아이콘" />
-          <p>Mypage</p>
-        </button>
-      </div>
-      <Link to="/home">
-        <img className="logoImg" src={LogoImg} alt="로고이미지" />
-      </Link>
-      {openUploadModal && <Upload setOpenPopup={setOpenUploadModal} />}
-      {openMyModal && <MyPopup setOpenPopup={setOpenMyModal} />}
-    </StyledNav>
+    <>
+      {clientWitch > 430 && (
+        <StyledNav>
+          <div className="navBtn">
+            <Link to="/home">
+              <button type="button" className="home">
+                <img src={HomeImg} alt="홈 아이콘" />
+                <p>Home</p>
+              </button>
+            </Link>
+            <button
+              type="button"
+              className="upload"
+              onClick={openUploadModalFunc}
+            >
+              <img src={UploadImg} alt="업로드 아이콘" />
+              <p>Upload</p>
+            </button>
+            <button type="button" className="my" onClick={openMyModalFunc}>
+              <img src={MypageImg} alt="마이페이지 아이콘" />
+              <p>Mypage</p>
+            </button>
+          </div>
+          <Link to="/home">
+            <img className="logoImg" src={LogoImg} alt="로고이미지" />
+          </Link>
+          {openUploadModal && <Upload setOpenPopup={setOpenUploadModal} />}
+          {openMyModal && <MyPopup setOpenPopup={setOpenMyModal} />}
+        </StyledNav>
+      )}
+    </>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,6 +8,8 @@ import Album from '../../components/Album/Album';
 import { useState } from 'react';
 import NewAlbumModal from '../../components/Modal/NewAlbumModal';
 import ArrayModal from '../../components/Modal/ArrayModal';
+import MobileHeader from './MobileHeader';
+
 export default function Home() {
   const [isArrayModalOpen, setIsArrayModalOpen] = useState(false);
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
@@ -26,27 +28,30 @@ export default function Home() {
     setIsAddModalOpen(false);
   };
   return (
-    <StyledMain>
-      <StyledHomeSection>
-        <StyledH2>Album</StyledH2>
-        <div className="btn-wrap">
-          <button onClick={HandleArrayModal}>
-            <img src={ArrayImg} alt="정렬방식 아이콘" />
-          </button>
-          <button onClick={HandleAddModal}>
-            <img src={AddImg} alt="이미지 추가 아이콘" />
-          </button>
-        </div>
-        <ul>
-          <li>
-            <Link to="/home">
-              <Album />
-            </Link>
-          </li>
-        </ul>
-      </StyledHomeSection>
-      {isAddModalOpen && <NewAlbumModal onClose={HandleAddCloseModal} />}
-      {isArrayModalOpen && <ArrayModal onClose={HandleArrayCloseModal} />}
-    </StyledMain>
+    <>
+      <MobileHeader />
+      <StyledMain>
+        <StyledHomeSection>
+          <StyledH2>Album</StyledH2>
+          <div className="btn-wrap">
+            <button type="button" onClick={HandleArrayModal}>
+              <img src={ArrayImg} alt="정렬방식 아이콘" />
+            </button>
+            <button type="button" onClick={HandleAddModal}>
+              <img src={AddImg} alt="이미지 추가 아이콘" />
+            </button>
+          </div>
+          <ul>
+            <li>
+              <Link to="/home">
+                <Album />
+              </Link>
+            </li>
+          </ul>
+        </StyledHomeSection>
+        {isAddModalOpen && <NewAlbumModal onClose={HandleAddCloseModal} />}
+        {isArrayModalOpen && <ArrayModal onClose={HandleArrayCloseModal} />}
+      </StyledMain>
+    </>
   );
 }
