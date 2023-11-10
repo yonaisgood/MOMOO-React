29280d9e0219adbee775473b00eb94a86b0cd1b4
yonainthefish <yonaisgood@gmail.com>
2023-11-10 18:39:41 +0900

    :recycle: Refactor: accordion 데이터 작동방식에 따라 분리(#59)

---
+++ src/components/Accordion/MultipleAccordion.tsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import { AccordionWrapper, MultiAccordionWrapper } from './AccordionStyle';
+import Direction from '../../asset/icon/Arrow.svg';
+
+interface AccordionProps {
+  question: string;
+  answer: string;
+}
+
+function MultipleAccordion({ question, answer }: AccordionProps) {
+  const [isAccordionOpen, setIsAccordionOpen] = useState(false);
+  const answerArray = answer.split(',');
+  const [selectedTexts, setSelectedTexts] = useState<string[]>([
+    answerArray[0],
+  ]);
+
+  const handleQuestionClick = () => {
+    setIsAccordionOpen((prev) => !prev);
+  };
+
+  const MultiAnswerClick = (text: string) => {
+    // 이미 선택된 텍스트인지 확인
+    const isSelected = selectedTexts.includes(text);
+
+    if (text === answerArray[0]) {
+      return;
+    }
+
+    if (isSelected) {
+      const updatedSelection = selectedTexts.filter(
+        (selected) => selected !== text,
+      );
+      setSelectedTexts(updatedSelection);
+    } else {
+      // 선택되지 않은 텍스트일 경우 선택 상태로 업데이트
+      setSelectedTexts([...selectedTexts, text]);
+    }
+  };
+
+  return (
+    <AccordionWrapper>
+      <div id="Accordion_wrap">
+        <div className="que" onClick={handleQuestionClick}>
+          <span>{question}</span>
+          <div className="arrow-wrap">
+            <span className={isAccordionOpen ? 'arrow-top' : 'arrow-bottom'}>
+              <img
+                className="directionIcon"
+                src={Direction}
+                alt="Arrow Direction"
+              />
+            </span>
+          </div>
+        </div>
+
+        {isAccordionOpen && (
+          <MultiAccordionWrapper>
+            <div className="anw" id="multiAnswer">
+              {answerArray.map((item, index) => (
+                <button
+                  key={index}
+                  onClick={() => MultiAnswerClick(item.trim())}
+                  className={selectedTexts.includes(item) ? 'selected' : ''}
+                >
+                  {item}
+                </button>
+              ))}
+            </div>
+          </MultiAccordionWrapper>
+        )}
+      </div>
+    </AccordionWrapper>
+  );
+}
+
+export default MultipleAccordion;
--- src/components/Upload/accordionData.ts
+++ src/components/Upload/accordionData.ts
@@ -1,29 +1,46 @@
-const accordionData = [
-  {
-    question: '앨범 선택',
-    answer: [],
-  },
-  {
-    question: '오늘의 날씨',
-    answer: [
-      '/src/asset/image/Sunny.svg',
-      '/src/asset/image/PartlySunny.svg',
-      '/src/asset/image/Cloudy.svg',
-      '/src/asset/image/Rainy.svg',
-      '/src/asset/image/Snowy.svg',
-    ],
-  },
-  {
-    question: '오늘의 기분',
-    answer: [
-      '/src/asset/image/Excited.svg',
-      '/src/asset/image/Smiling.svg',
-      '/src/asset/image/Yummy.svg',
-      '/src/asset/image/Frowning.svg',
-      '/src/asset/image/Sad.svg',
-      '/src/asset/image/Angry.svg',
-    ],
-  },
-];
+import { getDocs, collection, query, orderBy } from 'firebase/firestore';
+import { appFireStore } from '../../firebase/config';
+import { User } from 'firebase/auth';
 
-export default accordionData;
+const GetAccordionData = async (user: User) => {
+  const accordionData = [
+    {
+      question: '앨범 선택',
+      answer: [],
+    },
+    {
+      question: '오늘의 날씨',
+      answer: [
+        '/src/asset/image/Sunny.svg',
+        '/src/asset/image/PartlySunny.svg',
+        '/src/asset/image/Cloudy.svg',
+        '/src/asset/image/Rainy.svg',
+        '/src/asset/image/Snowy.svg',
+      ],
+    },
+    {
+      question: '오늘의 기분',
+      answer: [
+        '/src/asset/image/Excited.svg',
+        '/src/asset/image/Smiling.svg',
+        '/src/asset/image/Yummy.svg',
+        '/src/asset/image/Frowning.svg',
+        '/src/asset/image/Sad.svg',
+        '/src/asset/image/Angry.svg',
+      ],
+    },
+  ];
+
+  const albumDocRef = collection(appFireStore, user.uid, user.uid, 'album');
+  const orderedAlbums = query(albumDocRef, orderBy('createdTime'));
+
+  // 해당 문서의 데이터 가져오기
+  const querySnapshot = await getDocs(orderedAlbums);
+  querySnapshot.forEach((doc) => {
+    accordionData[0].answer.push(doc.data().name);
+  });
+
+  return accordionData;
+};
+
+export default GetAccordionData;
