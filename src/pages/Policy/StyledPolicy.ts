1ef3bf5d97bd2a3b2317c9e062c934f9be57a0b8
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2024-01-25 17:37:45 +0900

    :books: Docs: 이용약관/개인정보 처리방침 수정(#182)

--- src/pages/Policy/StyledPolicy.ts
+++ src/pages/Policy/StyledPolicy.ts
@@ -1,32 +1,58 @@
 import styled from 'styled-components';
 
 const StyledPolicy = styled.main`
-  max-width: 1024px;
-  box-sizing: content-box;
+  width: 100%;
   color: var(--gray-900);
   font-size: var(--text-m);
 
-  section {
-    margin-top: 52px;
+  h2 {
+    margin-bottom: 52px;
+  }
 
-    strong:not(:first-child) {
-      margin-top: 44px;
+  & > p {
+    margin: 0 auto 40px;
+    max-width: 976px;
+  }
+
+  section {
+    max-width: 976px;
+    margin: 0 auto;
+
+    a {
+      border-bottom: 1px solid var(--gray-600);
+    }
+
+    li + li {
+      margin-top: 40px;
+    }
+
+    h4 {
       display: block;
       font-size: var(--text-l);
+      margin-bottom: 10px;
     }
 
-    li,
-    p {
-      margin-top: 12px;
+    p + p {
+      margin-top: 4px;
     }
+  }
 
-    ol ol {
-      padding: 4px 0 0 20px;
+  ul {
+    margin-top: 10px;
 
-      li {
-        margin-top: 6px;
-        font-size: var(--text-s);
-      }
+    li + li {
+      margin-top: 10px;
+      padding-top: 0;
+      border-top: none;
+    }
+  }
+
+  ul ul {
+    margin: 10px 0 0 16px;
+    font-size: var(--text-s);
+
+    li + li {
+      margin-top: 6px;
     }
   }
 
@@ -37,17 +63,33 @@ const StyledPolicy = styled.main`
     margin: 0 auto;
     padding: calc(var(--nav-height-tablet)) 24px 32px;
 
+    h2 {
+      margin-bottom: 0;
+    }
+
+    & > p {
+      margin: 40px auto 0;
+    }
+
     section {
-      margin-top: 32px;
+      margin-top: 40px;
     }
   }
 
   @media (max-width: 430px) {
     margin: 0 auto;
-    padding: calc(var(--nav-height-mobile) + 32px) 16px;
+    padding: calc(var(--nav-height-mobile) + 36px) 16px;
+
+    & > p {
+      margin: 0 auto 30px;
+    }
 
     section {
       margin-top: 0;
+    }
+
+    ol > li + li {
+      margin-top: 30px;
     }
   }
 `;
