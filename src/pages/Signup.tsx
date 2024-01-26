b2ce6dffc0c354e2bd6f33e409138d91d13d39ab
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2024-01-26 15:12:09 +0900

    :rocket: Release: 빌드 및 재배포

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import useSignup from '../hooks/useSingup.ts';
