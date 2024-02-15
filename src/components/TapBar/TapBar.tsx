270938caf1635d0ee1eb806c8d0c7cd91535da98
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2024-02-15 19:35:28 +0900

    :bug: Fix: a/button 하나만 사용/a 태그로 통일(#237)

--- src/components/TapBar/TapBar.tsx
+++ src/components/TapBar/TapBar.tsx
@@ -1,4 +1,4 @@
-import { useLocation, Link, useNavigate } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 import StyledNav from './StyledTapBar';
 import HomeImg from '../../asset/icon/HomeMobile.svg';
 import HomeActiveImg from '../../asset/icon/HomeMobile-Active.svg';
@@ -8,38 +8,29 @@ import MypageActiveImg from '../../asset/icon/ProfileMobile-Active.svg';
 
 export default function TabBar() {
   const location = useLocation();
-  const navigate = useNavigate();
 
   return (
     <StyledNav>
       <div className="navBtn">
-        <button type="button">
-          <Link to="/" className="homeLink">
-            <img
-              src={location.pathname === '/' ? HomeActiveImg : HomeImg}
-              alt="홈 아이콘"
-            />
-            <p
-              style={{
-                color:
-                  location.pathname === '/'
-                    ? 'var(--point-dark-400)'
-                    : 'inherit',
-              }}
-            >
-              Home
-            </p>
-          </Link>
-        </button>
-        <button
-          type="button"
-          className="upload"
-          onClick={() => navigate('/upload')}
-        >
+        <Link to="/" className="homeLink">
+          <img
+            src={location.pathname === '/' ? HomeActiveImg : HomeImg}
+            alt="홈 아이콘"
+          />
+          <p
+            style={{
+              color:
+                location.pathname === '/' ? 'var(--point-dark-400)' : 'inherit',
+            }}
+          >
+            Home
+          </p>
+        </Link>
+        <Link className="upload" to="/upload">
           <img src={UploadImg} alt="업로드 아이콘" />
           <p>Upload</p>
-        </button>
-        <button type="button" className="my" onClick={() => navigate('/my')}>
+        </Link>
+        <Link className="my" to="/my">
           <img
             src={location.pathname === '/my' ? MypageActiveImg : MypageImg}
             alt="마이페이지 아이콘"
@@ -54,7 +45,7 @@ export default function TabBar() {
           >
             Mypage
           </p>
-        </button>
+        </Link>
       </div>
     </StyledNav>
   );
