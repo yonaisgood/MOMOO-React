3f6da3502a3cfb2d7c6c6d75c1d003dfbbffed47
suminson <sabi1111@nate.com>
2024-02-25 05:26:33 +0900

    :bug: 두 번 포커스현상 해결, 버튼 disabled속성 추가(#241)

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import useUploadContext from '../../hooks/useUploadContext';
+import useAuthContext from '../../hooks/useAuthContext';
 
 import MyNonModal from '../MyNonModal/MyNonModal';
 import StyledNav from './StyledNav';
@@ -17,9 +18,15 @@ export default function Nav() {
   const [clientWitch, setClientWitch] = useState(
     document.documentElement.clientWidth,
   );
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
   const { setIsUploadModalOpen } = useUploadContext();
+  const navigate = useNavigate();
+  const { user } = useAuthContext();
 
   useEffect(() => {
+    if (!user) {
+      setIsAuthenticated(false);
+    }
     window.addEventListener('resize', () => {
       setClientWitch(document.documentElement.clientWidth);
     });
@@ -32,38 +39,63 @@ export default function Nav() {
   const openMyDialogFunc = () => {
     setIsOpenMyDialog(true);
   };
+  const handleHomeNavigate = () => {
+    navigate('/');
+  };
 
   return (
     <>
       <StyledNav>
         <div className="navBtn">
-          <Link to="/">
-            <button type="button" className="home">
-              <img src={HomeImg} alt="홈 아이콘" />
-              <p>Home</p>
-            </button>
-          </Link>
+          <button
+            type="button"
+            className="home"
+            onClick={handleHomeNavigate}
+            disabled={!isAuthenticated}
+          >
+            <img src={HomeImg} alt="홈 아이콘" />
+            <p>Home</p>
+          </button>
+
           <button
             type="button"
             className="upload"
             onClick={openUploadModalFunc}
+            disabled={!isAuthenticated}
           >
             <img src={UploadImg} alt="업로드 아이콘" />
             <p>Upload</p>
           </button>
-          <button type="button" className="my" onClick={openMyDialogFunc}>
+          <button
+            type="button"
+            className="my"
+            onClick={openMyDialogFunc}
+            disabled={!isAuthenticated}
+          >
             <img src={MypageImg} alt="마이페이지 아이콘" />
             <p>Mypage</p>
           </button>
         </div>
-        <Link to="/">
-          <h1 className="a11y-hidden">MoMoo</h1>
-          <img
-            className="logoImg"
-            src={clientWitch > 1024 ? LogoColImg : LogoImg}
-            alt="로고이미지"
-          />
-        </Link>
+        {isAuthenticated ? (
+          <Link to="/">
+            <h1 className="a11y-hidden">MoMoo</h1>
+            <img
+              className="logoImg"
+              src={clientWitch > 1024 ? LogoColImg : LogoImg}
+              alt="로고이미지"
+            />
+          </Link>
+        ) : (
+          <>
+            <h1 className="a11y-hidden">MoMoo</h1>
+            <img
+              className="logoImg"
+              src={clientWitch > 1024 ? LogoColImg : LogoImg}
+              alt="로고이미지"
+            />
+          </>
+        )}
+
         {openMyDialog && <MyNonModal setIsDialogOpen={setIsOpenMyDialog} />}
       </StyledNav>
     </>
