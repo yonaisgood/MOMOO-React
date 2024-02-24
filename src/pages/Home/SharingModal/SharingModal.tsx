6e881094934aea7e7ea77799c6af043b3a130fdb
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2024-02-25 00:14:42 +0900

    :art: Design: 공유 모달 UI(#253)

---
+++ src/pages/Home/SharingModal/SharingModal.tsx
@@ -0,0 +1,67 @@
+// import useEscDialog from '../../../hooks/dialog/useEscDialog';
+// import useShowModal from '../../../hooks/dialog/useShowModal';
+
+import { StyledSharingModal, DialogTitle } from './StyledSharingModal';
+
+// import { closeDialogOnClick } from '../../../utils/dialog';
+
+import Close from '../../../asset/icon/X-Small.svg';
+import { useRef } from 'react';
+
+interface Props {
+  closeModal: () => void;
+}
+
+export default function SharingModal({ closeModal }: Props) {
+  // const { showModal } = useShowModal();
+  // useEscDialog(closeModal);
+  const ref = useRef<HTMLDialogElement | null>(null);
+  return (
+    <StyledSharingModal
+      aria-labelledby="modal"
+      ref={(node) => {
+        if (node && !ref.current) {
+          ref.current = node;
+          node.showModal();
+        }
+      }}
+      // ref={showModal}
+      // onClick={(e) => closeDialogOnClick(e, closeModal)}
+    >
+      <div>
+        <DialogTitle>공유</DialogTitle>
+        <div>
+          <label htmlFor="sharing" className="a11y-hidden">
+            공유 링크
+          </label>
+          <input id="sharing" type="url" value="https://momoo.kr" />
+          <button type="button" className="copy-btn">
+            복사
+          </button>
+        </div>
+        <strong className="manage">게스트 관리</strong>
+        <ul>
+          <li>
+            <img src={Close} alt="프로필 사진" />
+            <div>
+              <span className="ellipsis">애벌레가 먹은 사과는 맛있었다</span>
+              <span className="ellipsis">appleappleappleapple@naver.com</span>
+            </div>
+            <button type="button">삭제</button>
+          </li>
+          <li>
+            <img src={Close} alt="프로필 사진" />
+            <div>
+              <span className="ellipsis">김사과</span>
+              <span className="ellipsis">apple@naver.com</span>
+            </div>
+            <button type="button">삭제</button>
+          </li>
+        </ul>
+        <button type="button" className="close-button" onClick={closeModal}>
+          <img src={Close} alt="모달 닫기 버튼" />
+        </button>
+      </div>
+    </StyledSharingModal>
+  );
+}
