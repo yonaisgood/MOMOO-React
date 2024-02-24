c4ccf114eca06898bc929d8f7ce52a899d613d85
suminson <sabi1111@nate.com>
2024-02-24 22:37:09 +0900

    :bug: Fix: useDeleteAlbum,useEditAlbum전체 보기인지 확인하는 로직 제거(#240)

--- src/hooks/useDeleteAlbum.ts
+++ src/hooks/useDeleteAlbum.ts
@@ -32,18 +32,6 @@ export default function useDeleteAlbum() {
     try {
       setIsPending(true);
 
-      // "전체 보기" 앨범이라면 삭제를 막음
-      const albumDoc = await getDoc(userAlbumDocRef);
-      if (albumDoc.exists() && albumDoc.data().name === '전체 보기') {
-        setError('전체 보기 앨범은 삭제할 수 없습니다.');
-        return {
-          success: false,
-          message: '전체 보기 앨범은 삭제할 수 없습니다.',
-        };
-      }
-
-      // "전체 보기" 앨범이 아닌 경우에만 삭제 진행
-
       await deleteDoc(userAlbumDocRef);
       setIsPending(false);
 
