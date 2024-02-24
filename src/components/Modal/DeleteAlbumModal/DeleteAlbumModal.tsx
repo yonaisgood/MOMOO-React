2a7165c7b2c87b96c3ccd953ded9f5a0b0b4934e
suminson <sabi1111@nate.com>
2024-02-24 21:47:59 +0900

    :recycle: 앨범 삭제시 ConfirmModal알림 , 삭제 로딩중일때 로딩이미지, 실패시 Alert모달알림 (#240)

--- src/components/Modal/DeleteAlbumModal/DeleteAlbumModal.tsx
+++ src/components/Modal/DeleteAlbumModal/DeleteAlbumModal.tsx
@@ -3,6 +3,9 @@ import React, { useRef, useEffect, useState } from 'react';
 import useEditAlbum from '../../../hooks/useEditAlbum';
 import useDeleteAlbum from '../../../hooks/useDeleteAlbum';
 
+import LoadingComponent from '../../Loading/LoadingComponent';
+import ConfirmModal from '../ConfirmModal/ConfirmModal';
+import AlertModal from '../AlertModal/AlertModal';
 import { SelectModal, Header } from './StyledDeleteAlbumModal';
 import ModalOverlay from '../../CommonStyled/StyledModalOverlay';
 
@@ -24,8 +27,9 @@ const DeleteAlbumModal: React.FC<DeleteAlbumModalProps> = ({
   const closeButtonRef = useRef<HTMLButtonElement>(null);
   const [editAlbumName, setEditAlbumName] = useState(albumName);
   const [errMessage, setErrMessage] = useState('');
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
   const editAlbum = useEditAlbum();
-  const deleteAlbum = useDeleteAlbum();
+  const { deleteAlbum, isPending, error } = useDeleteAlbum();
 
   useEffect(() => {
     const handleKeydown = (event: KeyboardEvent) => {
@@ -58,23 +62,6 @@ const DeleteAlbumModal: React.FC<DeleteAlbumModalProps> = ({
     };
   }, []);
 
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        modalRef.current &&
-        !modalRef.current.contains(event.target as Node)
-      ) {
-        onClose();
-      }
-    };
-
-    document.addEventListener('mousedown', handleClickOutside);
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [onClose]);
-
   const handleEditAlbum = async () => {
     if (editAlbumName.trim() === '') {
       setErrMessage('제목을 입력해 주세요');
@@ -87,63 +74,86 @@ const DeleteAlbumModal: React.FC<DeleteAlbumModalProps> = ({
     }
     onClose();
   };
-  const handleDeleteAlbum = async () => {
-    await deleteAlbum({ albumId });
-    onClose();
+
+  const handleDeleteAlbum = () => {
+    setShowConfirmModal(true);
   };
+
+  const handleDeleteSuccess = async () => {
+    await deleteAlbum({ albumId });
+    if (error) {
+      setShowConfirmModal(false);
+      return <AlertModal message={'삭제를 실패하였습니다'} onClose={onClose} />;
+    }
+    setShowConfirmModal(false);
+  };
+
   return (
-    <SelectModal role="dialog" aria-labelledby="modal-select">
-      <ModalOverlay>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleEditAlbum();
-          }}
-        >
-          <div
-            className="modal-content"
-            role="document"
-            tabIndex={-1}
-            ref={modalRef}
+    <>
+      <SelectModal role="dialog" aria-labelledby="modal-select">
+        <ModalOverlay>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleEditAlbum();
+            }}
           >
-            <Header className="modal-header" id="modal-select">
-              <h2 tabIndex={0}>Edit Album</h2>
-            </Header>
-            <div className="modal-list">
-              <p>이름</p>
-              <input
-                type="text"
-                value={editAlbumName}
-                onChange={(e) => {
-                  setEditAlbumName(e.target.value);
-                }}
-                placeholder="새로운 앨범명을 입력해주세요"
-              />
-              {errMessage !== '' && <strong role="alert">*{errMessage}</strong>}
-              <button type="button" onClick={handleDeleteAlbum}>
-                Delete
-                <img src={DeleteRedImg} alt="휴지통 아이콘" />
+            <div
+              className="modal-content"
+              role="document"
+              tabIndex={-1}
+              ref={modalRef}
+            >
+              <Header className="modal-header" id="modal-select">
+                <h2 tabIndex={0}>Edit Album</h2>
+              </Header>
+              <div className="modal-list">
+                <p>이름</p>
+                <input
+                  type="text"
+                  value={editAlbumName}
+                  onChange={(e) => {
+                    setEditAlbumName(e.target.value);
+                  }}
+                  placeholder="새로운 앨범명을 입력해주세요"
+                />
+                {errMessage !== '' && (
+                  <strong role="alert">*{errMessage}</strong>
+                )}
+                <button type="button" onClick={handleDeleteAlbum}>
+                  Delete
+                  <img src={DeleteRedImg} alt="휴지통 아이콘" />
+                </button>
+              </div>
+              <button
+                onClick={handleEditAlbum}
+                className="edit-btn"
+                type="submit"
+              >
+                <img src={SelectImg} alt="수정" />
+              </button>
+              <button
+                className="close-button"
+                onClick={onClose}
+                tabIndex={0}
+                ref={closeButtonRef}
+              >
+                <img src={Close} alt="모달 닫기 버튼" />
               </button>
             </div>
-            <button
-              onClick={handleEditAlbum}
-              className="edit-btn"
-              type="submit"
-            >
-              <img src={SelectImg} alt="수정" />
-            </button>
-            <button
-              className="close-button"
-              onClick={onClose}
-              tabIndex={0}
-              ref={closeButtonRef}
-            >
-              <img src={Close} alt="모달 닫기 버튼" />
-            </button>
-          </div>
-        </form>
-      </ModalOverlay>
-    </SelectModal>
+          </form>
+        </ModalOverlay>
+      </SelectModal>
+      {showConfirmModal && (
+        <ConfirmModal
+          onClose={() => setShowConfirmModal(false)}
+          handleAgreeBtn={handleDeleteSuccess}
+          title={`앨범을 정말 삭제하시겠습니까?`}
+          btnNameList={['취소', '확인']}
+        />
+      )}
+      {isPending && <LoadingComponent />}
+    </>
   );
 };
 export default DeleteAlbumModal;
