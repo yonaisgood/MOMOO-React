a5fdb96c8dcd4eedac5c7c7393a1eddea074a842
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2024-02-28 21:03:26 +0900

    :bulb: Feat: albumDetail - 공유 앨범 데이터 불러오기/권한 검증/렌더링(#253) - useGetAlbumFeedList -> SDKUtils - getAlbumByName - useGetFeedListData -> useGetFeedsData - useGetSharedAlbumList -> useGetSharedAlbums + useGetSharedAlbumsData - Album(detail): 앨범 검증 및 피드 데이터 불러오는 로직 hook으로 분리

--- src/pages/Album/Album.tsx
+++ src/pages/Album/Album.tsx
@@ -1,86 +1,45 @@
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
-import { DocumentData } from 'firebase/firestore';
 
 import useUploadContext from '../../hooks/useUploadContext';
-import useGetAlbumFeedList from '../../hooks/useGetAlbumFeedList';
-import useGetFeedListData from '../../hooks/useGetFeedListData';
-import useSetFeedItemLayout from './useSetFeedItemLayout';
 
 import BreadcrumbWrap from '../../components/Breadcrumb/BreadcrumbWrap';
 import Breadcrumb from '../../components/Breadcrumb/Breadcrumb';
 import LoadingComponent from '../../components/Loading/LoadingComponent';
 import FeedItem from './FeedItem';
 import StyledH2 from '../../components/CommonStyled/StyledH2';
-import StyledAlbum, { StyledFeedList, StyledAddFeedItem } from './StyledAlbum';
+import StyledAlbum, { StyledFeedList } from './StyledAlbum';
 
 import AddIcon from '../../asset/icon/Add_L.svg';
 import TopBar from '../../components/Topbar/Topbar';
+import useSetAlbumData from './useSetAlbumData';
 
 export default function Album() {
   const [clientWitch, setClientWitch] = useState(
     document.documentElement.clientWidth,
   );
-  const [feedList, setFeedList] = useState<DocumentData[] | null>(null);
-  const [albumName, setAlbumName] = useState<null | string>(null);
-  const { uid, album } = useParams();
+
+  const { album } = useParams();
   const navigate = useNavigate();
 
-  const getAlbumFeedList = useGetAlbumFeedList();
-  const getFeedListData = useGetFeedListData();
   const { setAlbumNameListToAdd, setIsUploadModalOpen } = useUploadContext();
-  const { imgRatio, setRatio, setGridRowEnd } = useSetFeedItemLayout();
+  const { feedsData } = useSetAlbumData();
+
+  if (!album) {
+    navigate('/404');
+    return;
+  }
 
   useEffect(() => {
-    if (!uid || !album) {
-      navigate('/404');
-      return;
-    }
-
-    // 엑세스 권한 없을 경우 로직 추가하기
-
     window.addEventListener('resize', () => {
       setClientWitch(document.documentElement.clientWidth);
     });
-
-    const setAlbumData = async () => {
-      const albumNameToSet = album.replace(/-/gi, ' ');
-      const albumFeedList = await getAlbumFeedList(albumNameToSet, uid);
-
-      if (!albumFeedList) {
-        navigate('/404');
-        return;
-      }
-
-      setAlbumName(albumNameToSet);
-
-      if (!albumFeedList.length) {
-        setFeedList([]);
-        setRatio(null);
-        return;
-      }
-
-      const feedListData = await getFeedListData(albumFeedList, uid);
-
-      if (!feedListData?.length) {
-        setFeedList([]);
-        setRatio(null);
-        return;
-      }
-
-      setFeedList(feedListData);
-      setRatio(feedListData[feedListData.length - 1].imageUrl[0]);
-    };
-
-    (async () => {
-      setAlbumData();
-    })();
   }, []);
 
   const openUpload = () => {
-    if (albumName && albumName !== '전체 보기') {
-      setAlbumNameListToAdd(['전체 보기', albumName]);
+    if (album !== '전체 보기') {
+      setAlbumNameListToAdd(['전체 보기', album]);
     }
 
     if (clientWitch > 430) {
@@ -92,66 +51,49 @@ export default function Album() {
 
   return (
     <>
-      <Helmet>{albumName && <title>{albumName} | MOMOO</title>}</Helmet>
+      <Helmet>{album && <title>{album} | MOMOO</title>}</Helmet>
 
-      {albumName && (
-        <>
-          {clientWitch <= 430 && <TopBar tit={albumName} />}
-          <StyledAlbum>
-            {clientWitch > 1024 && (
-              <>
-                <StyledH2>{albumName}</StyledH2>
-                <Breadcrumb
-                  navList={[
-                    { path: '/', text: 'Home' },
-                    { path: '', text: albumName },
-                  ]}
-                />
-              </>
-            )}
-            {clientWitch > 430 && clientWitch <= 1024 && (
-              <BreadcrumbWrap
-                navList={[
-                  { path: '/', text: 'Home' },
-                  { path: '', text: albumName },
-                ]}
-                title={albumName || ''}
-              />
-            )}
-            <section>
-              <h3 className="a11y-hidden">게시글 목록</h3>
-              {!feedList && <LoadingComponent />}
-              {feedList && (
-                <StyledFeedList>
-                  {feedList.length > 0 &&
-                    feedList.map((v) => {
-                      return <FeedItem key={v.id} feedData={v}></FeedItem>;
-                    })}
-                  {/* 현재 앨범에 사진 추가 UI 변경 예정 */}
-                  {imgRatio.width && imgRatio.height && (
-                    <StyledAddFeedItem
-                      $ratio={imgRatio.width / imgRatio.height}
-                      ref={(node) => {
-                        if (node) {
-                          setGridRowEnd(node);
-                        }
-                      }}
-                    >
-                      <button
-                        type="button"
-                        aria-label="새 게시물"
-                        onClick={openUpload}
-                      >
-                        <img src={AddIcon} alt="추가하기" />
-                      </button>
-                    </StyledAddFeedItem>
-                  )}
-                </StyledFeedList>
-              )}
-            </section>
-          </StyledAlbum>
-        </>
-      )}
+      {clientWitch <= 430 && <TopBar tit={album} />}
+      <StyledAlbum>
+        {clientWitch > 1024 && (
+          <>
+            <StyledH2>{album}</StyledH2>
+            <Breadcrumb
+              navList={[
+                { path: '/', text: 'Home' },
+                { path: '', text: album },
+              ]}
+            />
+          </>
+        )}
+        {clientWitch > 430 && clientWitch <= 1024 && (
+          <BreadcrumbWrap
+            navList={[
+              { path: '/', text: 'Home' },
+              { path: '', text: album },
+            ]}
+            title={album}
+          />
+        )}
+
+        {/* 현재 앨범에 사진 추가 UI 변경하기 */}
+        {/* <button type="button" aria-label="새 게시물" onClick={openUpload}>
+          <img src={AddIcon} alt="추가하기" />
+        </button> */}
+
+        <section>
+          <h3 className="a11y-hidden">게시글 목록</h3>
+          {!feedsData && <LoadingComponent />}
+          {feedsData && (
+            <StyledFeedList>
+              {feedsData.length > 0 &&
+                feedsData.map((v) => {
+                  return <FeedItem key={v.id} feedData={v}></FeedItem>;
+                })}
+            </StyledFeedList>
+          )}
+        </section>
+      </StyledAlbum>
     </>
   );
 }
