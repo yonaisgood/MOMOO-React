c82b470d89e5737b02e9ed69fe315bb1a861bf9b
yonainthefish <yonaisgood@gmail.com>
2024-03-11 14:03:44 +0900

    :art: Design: 변경된 업로드 UI 수정(#266)

--- src/components/FileUpload/Preview.tsx
+++ src/components/FileUpload/Preview.tsx
@@ -4,39 +4,43 @@ import AlertModal from '../../components/Modal/AlertModal/AlertModal';
 import * as Styled from './StyledPreview';
 
 import ImgUpload from '../../asset/icon/Add-L-White.svg';
-import ArrowWhite from '../../asset/icon/Arrow-White.svg';
+import DeleteImg from '../../asset/icon/X-Small.svg';
 
 const Preview = ({
-  setFile,
   imgUrlList,
 }: {
   setFile: Dispatch<SetStateAction<FileList | null>>;
   imgUrlList: string[];
 }) => {
   const [imageList, setImageList] = useState<string[]>([]);
-  const [currentIndex, setCurrentIndex] = useState(0);
+
   const [submitErrMessage, setSubmitErrMessage] = useState('');
 
   const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
+    const MAX_IMAGE_SIZE = 10 * 1024 * 1024;
 
     if (files && files.length > 0) {
-      const file = files[0];
-
-      if (!/^image\/(jpg|svg|png|jpeg|bmp|tif|heic)$/.test(file.type)) {
-        setSubmitErrMessage(
-          '이미지 파일 확장자는 jpg, svg, png, jpeg, bmp, tif, heic만 가능합니다.',
+      const validFiles = Array.from(files).filter((file) => {
+        const isValidType = /^image\/(jpg|svg|png|jpeg|bmp|tif|heic)$/.test(
+          file.type,
         );
-        return;
-      }
+        const isValidSize = file.size <= MAX_IMAGE_SIZE;
 
-      if (file.size > 10 * 1024 * 1024) {
-        setSubmitErrMessage('이미지 용량은 10MB 이내로 등록 가능합니다.');
-        return;
-      }
+        if (!isValidType || !isValidSize) {
+          setSubmitErrMessage(
+            !isValidType
+              ? '이미지 파일 확장자는 jpg, svg, png, jpeg, bmp, tif, heic만 가능합니다.'
+              : '이미지 용량은 10MB 이내로 등록 가능합니다.',
+          );
+          return false;
+        }
+        return true;
+      });
 
-      setImages(files);
-      setFile(files);
+      const newImages = validFiles.map((file) => URL.createObjectURL(file));
+
+      setImageList((prevImages) => [...prevImages, ...newImages]);
     } else {
       setSubmitErrMessage('이미지 파일을 선택해주세요.');
     }
@@ -46,119 +50,60 @@ const Preview = ({
     setImageList(imgUrlList);
   }, [imgUrlList]);
 
-  const setImages = async (files: FileList) => {
-    if (files) {
-      if (files.length <= 3) {
-        const fileArray = Array.from(files);
-        const newImages: string[] = [];
-
-        for (const file of fileArray) {
-          const imageUrl = URL.createObjectURL(file);
-          newImages.push(imageUrl);
-        }
-
-        setImageList(newImages);
-      } else {
-        setSubmitErrMessage('최대 3장의 이미지까지만 선택할 수 있습니다.');
-      }
-    }
-  };
-
-  const nextSlide = () => {
-    setCurrentIndex((currentIndex + 1) % imageList.length);
-  };
-
-  const prevSlide = () => {
-    setCurrentIndex((currentIndex - 1 + imageList.length) % imageList.length);
-  };
-
-  const handleIndicatorClick = (index: number) => {
-    setCurrentIndex(index);
+  const handleRemoveImage = (indexToRemove: number) => {
+    setImageList((currentImgList) =>
+      currentImgList.filter((_, index) => index !== indexToRemove),
+    );
   };
 
   return (
     <>
-      <Styled.PreviewSection>
-        {imageList.length === 0 && (
-          <label htmlFor="file">
-            <div className="uploadText">
-              <p>사진을 선택해 주세요(필수)</p>
-              <span>*3장까지 업로드 가능</span>
+      <Styled.SelectContainer>
+        <Styled.ImageGrid>
+          {imageList.map((image, index) => (
+            <div key={index} className="selectedImgList">
+              <img className="seletedImg" src={image} alt="이미지" />
+              <button
+                className="deleteBtn"
+                onClick={() => handleRemoveImage(index)}
+              >
+                <img
+                  className="deleteBtnImg"
+                  src={DeleteImg}
+                  alt="이미지삭제"
+                />
+              </button>
+            </div>
+          ))}
+          <Styled.PreviewSection>
+            <label htmlFor="file">
               <img
                 src={ImgUpload}
                 alt="사진 업로드 버튼"
                 className="btnUpload"
               />
-            </div>
-          </label>
-        )}
-        <input
-          accept="image/*"
-          multiple
-          type="file"
-          id="file"
-          onChange={handleImageUpload}
-          onClick={(e) => console.log('file', e.currentTarget.files)}
-        />
-        <Styled.PreviewSlider>
-          {imageList.length > 0 && (
-            <>
-              {/* 모바일 슬라이드 */}
-              <Styled.ImageGrid>
-                {imageList.map((image, index) => (
-                  <img key={index} src={image} alt="이미지" />
-                ))}
-              </Styled.ImageGrid>
-              {/* 모바일 이상 슬라이드 */}
-              <Styled.ImgSlidePcSize>
-                {imageList.length > 1 && (
-                  <button onClick={prevSlide} className="ArrowBack">
-                    <img src={ArrowWhite} alt="뒤로가기 버튼" />
-                  </button>
-                )}
-                <img
-                  className="selectImg"
-                  src={imageList[currentIndex]}
-                  alt="이미지"
-                />
-                {imageList.length > 1 && (
-                  <button
-                    type="button"
-                    onClick={nextSlide}
-                    className="ArrowRight"
-                  >
-                    <img src={ArrowWhite} alt="앞으로가기 버튼" />
-                  </button>
-                )}
-              </Styled.ImgSlidePcSize>
-            </>
-          )}
-        </Styled.PreviewSlider>
-        <Styled.IndicatorBasicBox>
-          {imageList.length > 1 && (
-            <Styled.IndicatorContainer>
-              {imageList.map((_, index) => (
-                <Styled.Indicator
-                  key={index}
-                  active={index === currentIndex}
-                  onClick={() => handleIndicatorClick(index)}
-                />
-              ))}
-            </Styled.IndicatorContainer>
-          )}
-        </Styled.IndicatorBasicBox>
-      </Styled.PreviewSection>
+            </label>
+            <input
+              accept="image/*"
+              multiple
+              type="file"
+              id="file"
+              onChange={handleImageUpload}
+              onClick={(e) => console.log('file', e.currentTarget.files)}
+            />
+          </Styled.PreviewSection>
+        </Styled.ImageGrid>
 
-      {submitErrMessage && (
-        <AlertModal
-          message={submitErrMessage}
-          onClose={() => setSubmitErrMessage('')}
-        />
-      )}
+        {submitErrMessage && (
+          <AlertModal
+            message={submitErrMessage}
+            onClose={() => setSubmitErrMessage('')}
+          />
+        )}
+      </Styled.SelectContainer>
     </>
   );
 };
-
 Preview.defaultProps = {
   imgUrlList: [],
 };
